8b408f8d43faac851365164215c39ecc96e3ee50
Yoeli Barthel <yoelibarthel@MacBookPro.lan>
2025-04-14 18:36:04 +0300

    Fix: déplacement de tailwindcss en dépendance principale et amélioration du build script

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 }
